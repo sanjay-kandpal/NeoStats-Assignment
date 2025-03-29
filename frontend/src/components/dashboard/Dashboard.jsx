240bf011302e06e959e0f443bc7593177a64dcac
sanjay <72781686+sanjay-kandpal@users.noreply.github.com>
2025-03-29 13:54:37 +0530

    Fixed Dashboard visual

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -55,24 +55,48 @@ const Dashboard = () => {
         <>
         
 
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+
+
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                  {user.email}
+                <th scope="col" class="px-6 py-3">
+                    Username
                 </th>
                 <th scope="col" class="px-6 py-3">
-                 {user.username}
+                    <div class="flex items-center">
+                        Email
+                        <a href="#"><svg class="w-3 h-3 ms-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M8.574 11.024h6.852a2.075 2.075 0 0 0 1.847-1.086 1.9 1.9 0 0 0-.11-1.986L13.736 2.9a2.122 2.122 0 0 0-3.472 0L6.837 7.952a1.9 1.9 0 0 0-.11 1.986 2.074 2.074 0 0 0 1.847 1.086Zm6.852 1.952H8.574a2.072 2.072 0 0 0-1.847 1.087 1.9 1.9 0 0 0 .11 1.985l3.426 5.05a2.123 2.123 0 0 0 3.472 0l3.427-5.05a1.9 1.9 0 0 0 .11-1.985 2.074 2.074 0 0 0-1.846-1.087Z"/>
+  </svg></a>
+                    </div>
                 </th>
-                <th scope="col" class="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-                  <button className='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900'>Delete</button> 
-                </th> 
+                
+                
+                <th scope="col" class="px-6 py-3">
+                    <button class="sr-only">Delete</button>
+                </th>
             </tr>
         </thead>
-        
+          <tbody>
+              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                  <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      {user.username}
+                  </th>
+                  <td class="px-6 py-4">
+                      {user.email}
+                  </td>
+                  
+                  <td class="px-6 py-4 text-right">
+                      <button href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Delete</button>
+                  </td>
+              </tr>
+              
+          </tbody>
     </table>
 </div>
+
 
         </>
       ))}
